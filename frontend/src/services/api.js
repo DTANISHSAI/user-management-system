0b834bf7b7f666be30d0184221406ca266ba7908
tanishsaidusanapudi <dtanishsai@gmail.com>
2026-05-23 00:49:40 +0530

    connect frontend to backend + jwt interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,31 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-
-    baseURL: "http://localhost:8080",
+    baseURL: "https://usermanagementsystem.up.railway.app",
 });
 
-// ADD JWT TOKEN AUTOMATICALLY
-api.interceptors.request.use(
+// 🔐 Attach token automatically
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
 
-    (config) => {
-
-        const token =
-            localStorage.getItem("token");
-
-        if (token) {
-
-            config.headers.Authorization =
-                `Bearer ${token}`;
-        }
-
-        return config;
-    },
-
-    (error) => {
-
-        return Promise.reject(error);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
-);
+
+    return config;
+});
 
 export default api;
